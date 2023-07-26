a533b01e971a3e26d206938b974573af0a0982f0
@hexlet/cli <support@hexlet.io>
2023-07-26 15:59:24 +0300

    submit java-collections-ru/lambdas

---
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -0,0 +1,14 @@
+package exercise;
+
+import java.awt.*;
+import java.util.Arrays;
+
+// BEGIN
+/*class App {
+    public static String[][] enlargeArrayImage(String[][] image) {;
+        return Arrays.asList(image).stream()
+
+                .toArray(String[][]::new);
+    }
+}*/
+// END
