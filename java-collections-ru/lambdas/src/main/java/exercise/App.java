357a117393d70bf85bcb7df1bf3c3e21de409ede
@hexlet/cli <support@hexlet.io>
2023-07-26 16:22:49 +0300

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/App.java
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -1,14 +1,25 @@
 package exercise;
 
-import java.awt.*;
 import java.util.Arrays;
 
 // BEGIN
-/*class App {
-    public static String[][] enlargeArrayImage(String[][] image) {;
-        return Arrays.asList(image).stream()
+class App {
+    public static String[] duplicateValues(String[] items) {
 
+        return Arrays.stream(items)
+                .flatMap(item -> Arrays.stream(new String[] {item, item}))
+                .toArray(String[]::new);
+    }
+
+    public static String[][] enlargeArrayImage(String[][] image) {
+
+        String[][] horizontallyStretched = Arrays.stream(image)
+                .map(App::duplicateValues)
+                .toArray(String[][]::new);
+
+        return Arrays.stream(horizontallyStretched)
+                .flatMap(item -> Arrays.stream(new String[][] {item, item}))
                 .toArray(String[][]::new);
     }
-}*/
+}
 // END
