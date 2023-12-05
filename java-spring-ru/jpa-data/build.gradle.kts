a8dbc03c6853817cae88b117f9848b169705807b
@hexlet/cli <support@hexlet.io>
2023-12-05 16:54:01 +0400

    submit java-spring-ru/jpa-data

---
+++ java-spring-ru/jpa-data/build.gradle.kts
@@ -0,0 +1,45 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    application
+    id("org.springframework.boot") version "3.1.3"
+    id("io.spring.dependency-management") version "1.1.3"
+    id("com.github.ben-manes.versions") version "0.48.0"
+    id("io.freefair.lombok") version "8.3"
+}
+
+group = "exercise"
+
+version = "1.0-SNAPSHOT"
+
+application { mainClass.set("exercise.Application") }
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // BEGIN
+    runtimeOnly("com.h2database:h2:2.1.214")
+    // END
+    implementation("org.springframework.boot:spring-boot-starter:3.1.0")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.1.0")
+    implementation("org.springframework.boot:spring-boot-devtools:3.0.4")
+    // Зависимость нужна для работы механизма Spring Data JPA
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.0.4")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.0")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testImplementation("org.hamcrest:hamcrest:2.2")
+    testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        showStandardStreams = true
+    }
+}
